3b90cfe720d63aaa379510e5fc1dfd26a936b15c
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-12 15:54:21 +0100

    feat: snack 1 completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 // Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 // Stampare a schermo oppure in console la bici con peso minore.
 
-const bicicles = [
+const bicycles = [
     {
         name: "Atala",
         weight: 20
@@ -22,9 +22,17 @@ const bicicles = [
     }
 ];
 
-const lightweights = [];
-for (let i = 0; i < bicicles.length; i++) {
-    const bike = bicicles[i];
-    lightweights.push(bike.weight);
+// partiamo dal presupposto che sia la prima la più leggera
+let lightweight = bicycles[0];
+
+// ciclo per estrarre il peso delle altre 3
+for (let i = 1; i < bicycles.length; i++) {
+    const bike = bicycles[i];
+
+    // se è minore allora stampo
+    if (bike.weight < lightweight.weight) {
+        lightweight = bike;
+    }
 }
-console.log(lightweights)
+console.log(lightweight);
+
